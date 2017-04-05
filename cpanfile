2b433f65f5c5eb38d0c3761dd1008d0766d312b8
Ferenc Erki <ferki@adjust.com>
2017-04-05 14:15:05 +0200

    Initial commit

---
+++ cpanfile
@@ -0,0 +1,18 @@
+requires "Rex" => "0";
+requires "strict" => "0";
+requires "warnings" => "0";
+
+on 'configure' => sub {
+  requires "ExtUtils::MakeMaker" => "0";
+};
+
+on 'develop' => sub {
+  requires "English" => "0";
+  requires "File::Spec" => "0";
+  requires "IO::Handle" => "0";
+  requires "IPC::Open3" => "0";
+  requires "Test::More" => "0";
+  requires "Test::Pod" => "1.41";
+  requires "blib" => "1.01";
+  requires "perl" => "5.006";
+};
---
+++ lib/Rex/Flink.pm
@@ -0,0 +1,49 @@
+package Rex::Flink;
+
+# ABSTRACT: automate Flink tasks with Rex
+
+use strict;
+use warnings;
+
+our $VERSION = '9999';
+
+use Rex -feature => [qw(1.4 exec_autodie)];
+
+1;
+
+__END__
+
+=head1 SYNOPSIS
+
+    use Rex::Flink;
+
+=head1 DESCRIPTION
+
+=task
+
+=sub
+
+=head1 DIAGNOSTICS
+
+This module does not do any error checking (yet).
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+This module does not require any configuration, nor does it use any environment variables.
+
+=head1 DEPENDENCIES
+
+This module depends on the modules detailed below.
+
+CPAN modules:
+
+=for :list
+* Rex
+
+=head1 INCOMPATIBILITIES
+
+There are no known incompatibilities with other modules.
+
+=head1 BUGS AND LIMITATIONS
+
+There are no known bugs. Make sure they are reported.
